b8dc15f4c04951b36d281f17c9c1d82754485719
Ann310-front <a-konoplina@mail.ru>
2026-04-09 15:21:02 +0300

    2 tesk new 4

--- src/parsers.js
+++ src/parsers.js
@@ -1,28 +1,27 @@
 import fs from 'fs';
 import path from 'path';
 import { cwd } from 'process';
+import jsYaml from 'js-yaml';
 
-const getAbsolutePath = (filepath) => path.resolve(cwd(), filepath)
-
-const readFile = (filepath) => fs.readFileSync(absolutePath, 'utf-8');
-
+const getAbsolutePath = (filepath) => path.resolve(cwd(), filepath);
+const readFile = (filepath) => {
+  const absolutePath = getAbsolutePath(filepath);
+  return fs.readFileSync(absolutePath, 'utf-8');
+};
 const parseJson = (data) => JSON.parse(data);
-
+const parseYaml = (data) => jsYaml.load(data);
 const parsers = {
   json: parseJson,
+  yml: parseYaml,
+  yaml: parseYaml,
 };
-
 const getFormat = (filepath) => path.extname(filepath).toLowerCase().slice(1);
-    
-    return ext.slice(1);
-};
 
 export const getData = (filepath) => {
-    const data = readFile(filepath);
-    const format = getFormat(filepath);
-    
-    if (!parsers[format]) {
-        throw new Error(`Unsupported format: ${format}`);
+  const data = readFile(filepath);
+  const format = getFormat(filepath);
+  if (!parsers[format]) {
+    throw new Error(`Unsupported format: ${format}`);
   }
   return parsers[format](data);
 };
